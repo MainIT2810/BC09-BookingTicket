bd41699da2dbf722e66bc5865adfd1bde61cd39e
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2021-10-08 20:32:38 +0700

    project đã được set up sẵn

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="md:container">
+        <div className="shadow-sm ..." >Box 1</div>
+        <div className="shadow ..." >Box 2</div>
+        <div className="shadow-md ..." >Box 3</div>
+        <div className="shadow-lg ..." >Box 4</div>
+        <div className="shadow-xl ..." >Box 5</div>
+        <div className="shadow-2xl ..." >Box 6</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
