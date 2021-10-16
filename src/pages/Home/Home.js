0bf07bdbd40bd658ceb4cf1a4b8f831aae80c86e
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2021-10-16 21:15:26 +0700

    Done Footer UI

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import HomeMenu from './HomeMenu/HomeMenu'
 //Kết nối redux
 import { useSelector, useDispatch } from 'react-redux'
-import Film from '../../components/Film/Film';
 import MultipleRowSlick from '../../components/RSlick/MultipleRowSlick'
+import { layDanhSachPhimAction } from '../../redux/actions/QuanLyPhimActions';
+import { layDanhSachHeThongRapAction } from '../../redux/actions/QuanLyRapAction';
+// import HomeCarousel from '../../templates/HomeTemplate/Layout/HomeCarousel/HomeCarousel';
 
 export default function Home(props) {
 
     const { arrFilm } = useSelector(state => state.QuanLyPhimReducer);
-    console.log('propsHome', props);
-    // props.match.params
+    const {heThongRapChieu} = useSelector(state => state.QuanLyRapReducer);
+    const dispatch = useDispatch();
+    console.log('propsHome', arrFilm);
 
+    
+    // props.match.params
     // const renderFilms = () => {
     //     return arrFilm.map((phim, index) => {
     //         return <Film key={index} />
@@ -18,8 +23,19 @@ export default function Home(props) {
 
     //     })
     // }
+
+    useEffect(()=>{
+        const action = layDanhSachPhimAction();
+        dispatch(action); //dispatch function từ thunk
+
+        dispatch(layDanhSachHeThongRapAction());
+
+    },[])
+    
     return (
         <div>
+            {/* <HomeCarousel /> */}
+
             <section className="text-gray-600 body-font" >
                 <div className="container px-5 py-24 mx-auto " >
 
@@ -31,7 +47,7 @@ export default function Home(props) {
             </section>
 
             <div className="mx-36">
-                <HomeMenu />
+                <HomeMenu  heThongRapChieu={heThongRapChieu}/>
 
             </div>
         </div>
--- src/redux/reducers/QuanLyPhimReducer.js
+++ src/redux/reducers/QuanLyPhimReducer.js
@@ -1,42 +1,64 @@
+import { SET_DANH_SACH_PHIM, SET_FILM_SAP_CHIEU,SET_FILM_DANG_CHIEU } from "../actions/types/QuanLyPhimType"
 
 
 
 const stateDefault = {
     arrFilm: [
         {
-            "maPhim": 5031,
-            "tenPhim": "The Croods: New Ag1",
-            "biDanh": "the-croods-new-ag1",
-            "trailer": "https://www.youtube.com/watch?v=0qaStyeKpLo&feature=emb_logo",
-            "hinhAnh": "https://movienew.cybersoft.edu.vn/hinhanh/the-croods-new-age_gp01.jpg",
-            "moTa": "Sinh tồn trong một thế giới tiền sử luôn rình rập hiểm nguy từ đủ loài quái thú hung dữ cho tới thảm họa ngày tận thế, Nhà Croods chưa từng một lần chùn bước. Nhưng giờ đây họ sẽ phải đối mặt với thử thách lớn nhất từ trước tới nay: chung sống với một gia đình khác. Để tìm kiếm một mái nhà an toàn hơn, Nhà Croods bắt đầu hành trình khám phá thế giới tiến tới những vùng đất xa xôi đầy tiềm năng. Một ngày nọ, họ tình cờ lạc vào một nơi yên bình có đầy đủ mọi tiện nghi hiện đại và biệt lập với tường vây bao quanh. Tưởng rằng mọi vấn đề trong cuộc sống sẽ được giải quyết thì Nhà Croods lại phải chấp nhận với sự thật rằng đã có một gia đình khác định cư ở đây đó chính là Nhà Bettermans.",
-            "maNhom": "GP01",
-            "ngayKhoiChieu": "2021-09-26T00:00:00",
-            "danhGia": 10,
-            "hot": false,
-            "dangChieu": true,
-            "sapChieu": false
-          },
-          {
-            "maPhim": 5032,
-            "tenPhim": "Cuộc Chiến Hủy Diệt - Skylines 2",
-            "biDanh": "cuoc-chien-huy-diet-skylines-2",
-            "trailer": "https://www.youtube.com/watch?v=mFgmcsRzvOo",
-            "hinhAnh": "https://movienew.cybersoft.edu.vn/hinhanh/cuoc-chien-huy-diet-skylines_gp01.jpg",
-            "moTa": "Khi một loại virus đe dọa biến những người ngoài hành tinh thân thiện đang sinh sống trên trái đất chống lại con người, Đội trưởng Rose Corley phải lãnh đạo một đội lính đánh thuê ưu tú đến thế giới ngoài hành tinh để cứu những gì còn lại của nhân loạiiii",
-            "maNhom": "GP01",
-            "ngayKhoiChieu": "2021-08-26T00:00:00",
-            "danhGia": 10,
+          "maPhim": 1282,
+          "tenPhim": "Ban tay diet quy",
+          "biDanh": "ban-tay-diet-quy",
+          "trailer": "https://www.youtube.com/embed/uqJ9u7GSaYM",
+          "hinhAnh": "http://movieapi.cyberlearn.vn/hinhanh/ban-tay-diet-quy.png",
+          "moTa": "Newlywed couple Ted and Tami-Lynn want to have a baby, but in order to qualify to be a parent, Ted will have to prove he's a person in a court of law.",
+          "maNhom": "GP00",
+          "ngayKhoiChieu": "2019-07-29T00:00:00",
+          "danhGia": 5,
+          "hot": true,
+          "dangChieu": false,
+          "sapChieu": true
+        },
+        {
+            "maPhim": 1282,
+            "tenPhim": "Ban tay diet quy",
+            "biDanh": "ban-tay-diet-quy",
+            "trailer": "https://www.youtube.com/embed/uqJ9u7GSaYM",
+            "hinhAnh": "http://movieapi.cyberlearn.vn/hinhanh/ban-tay-diet-quy.png",
+            "moTa": "Newlywed couple Ted and Tami-Lynn want to have a baby, but in order to qualify to be a parent, Ted will have to prove he's a person in a court of law.",
+            "maNhom": "GP00",
+            "ngayKhoiChieu": "2019-07-29T00:00:00",
+            "danhGia": 5,
             "hot": true,
             "dangChieu": false,
             "sapChieu": true
-          },
-    ]
+          }
+    ],
+    dangChieu: true,
+    sapChieu:true,
+    arrFilmDefault: []
+
 }
 
 export const QuanLyPhimReducer = (state=stateDefault,action ) => {
     switch(action.type) {
 
+        case SET_DANH_SACH_PHIM : {
+            state.arrFilm = action.arrFilm;
+            state.arrFilmDefault = state.arrFilm;
+            return {...state}
+        }
+        case SET_FILM_DANG_CHIEU: {
+            state.dangChieu = !state.dangChieu;
+
+            state.arrFilm = state.arrFilmDefault.filter(film => film.dangChieu === state.dangChieu );
+            return {...state}
+        }
+        case SET_FILM_SAP_CHIEU : {
+            state.sapChieu = !state.sapChieu;
+
+            state.arrFilm = state.arrFilmDefault.filter(film => film.sapChieu === state.sapChieu );
+            return {...state}
+        }
 
         default : return {...state}
     }
